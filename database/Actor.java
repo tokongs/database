a035dc189c602f39373154fd28953a2c7c2fd982
Tobias Kongsvik <tokongs98@gmail.com>
2020-03-09 11:13:33 +0100

    Add role list to actor

--- database/Actor.java
+++ database/Actor.java
@@ -11,27 +11,35 @@ import java.util.Date;
 
 public class Actor extends Employee {
 
-  public Actor(int employeeId) {
+  private ArrayList<String> roles = new ArrayList<String>();
+
+  public Actor(final int employeeId) {
     super(employeeId);
   }
 
-  public void initialize(Connection connection) {
+  private ArrayList<String> getRoles(){
+    return roles;
+  }
+
+  public void initialize(final Connection connection) {
     try {
-      Statement stmt = connection.createStatement();
-      ResultSet rs =
-          stmt.executeQuery("SELECT name, birthYear, originCountry FROM Employee WHERE EmployeeId="
+      final Statement stmt = connection.createStatement();
+      final ResultSet rs =
+          stmt.executeQuery("SELECT * FROM Employee WHERE EmployeeId="
               + employeeId + "INNER JOIN EmployeeActs USING(EmployeeId)");
       while (rs.next()) {
         name = rs.getString("name");
         birthYear = rs.getString("birthYear");
         originCountry = rs.getString("originCountry");
+        roles.add(rs.getString("role"));
       }
 
-    } catch (Exception e) {
+    } catch (final Exception e) {
       System.out.println("db error during select of Actor with id: " + employeeId + e);
       return;
     }
-
   }
+
+  
 
 }
